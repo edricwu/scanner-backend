b63cae678ba038199c7d1aa4065da40ca7aef46d
edricwu <edricwu008@gmail.com>
2020-09-07 22:12:19 +0800

    fix db connection

--- config/mysql.js
+++ config/mysql.js
@@ -1,14 +1,22 @@
 var mysql = require('mysql');
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'mysql',
+//     database: 'scanner_app',
+//     timezone: '+01:00'
+//   });
+
+const connection = mysql.createPool({
     host: 'us-cdbr-east-02.cleardb.com',
     user: 'b7412554ef8499',
     password: '4a240af9',
     database: 'heroku_d999f2664f85b0c',
     timezone: '+01:00'
-  });
-
-connection.connect(function(err) {
-    if (err) throw err;
 });
+
+// connection.connect(function (err) {
+//     if (err) throw err;
+// });
 
 module.exports = connection;
