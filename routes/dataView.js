959f0af3e3fbef7eaa80081f72f886961f169ed4
edricwu <edricwu008@gmail.com>
2020-09-14 23:27:23 +0800

    add jwt verification

--- routes/dataView.js
+++ routes/dataView.js
@@ -11,7 +11,7 @@ router.get("/bak/all_months", function(req, res, next) {
     var str = req.get('Authorization');
     // console.log(req);
     try {
-        // var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
+        var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
         db.query('SELECT DISTINCT MONTHNAME(date_added) AS Month, YEAR(date_added) AS Year from bak_log;', function(err, result) {
             console.log(result);
             res.send(result);
@@ -27,7 +27,7 @@ router.get("/semaian/all_months", function(req, res, next) {
     var str = req.get('Authorization');
     // console.log(req);
     try {
-        // var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
+        var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
         db.query('SELECT DISTINCT MONTHNAME(date_added) AS Month, YEAR(date_added) AS Year from semaian_log;', function(err, result) {
             console.log(result);
             res.send(result);
@@ -43,7 +43,7 @@ router.get("/bak/dates/:month/:year", function(req, res, next) {
     var str = req.get('Authorization');
     // console.log(req);
     try {
-        // var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
+        var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
         db.query('SELECT DISTINCT DATE_FORMAT(date_added, "%d/%m/%Y") AS Date FROM bak_log WHERE \
             (MONTHNAME(date_added), YEAR(date_added)) = (?, ?);', [req.params.month, req.params.year], function(err, result) {
             res.send(result);
@@ -59,7 +59,7 @@ router.get("/semaian/dates/:month/:year", function(req, res, next) {
     var str = req.get('Authorization');
     // console.log(req);
     try {
-        // var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
+        var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
         db.query('SELECT DISTINCT DATE_FORMAT(date_added, "%d/%m/%Y") AS Date FROM semaian_log WHERE \
             (MONTHNAME(date_added), YEAR(date_added)) = (?, ?);', [req.params.month, req.params.year], function(err, result) {
                 console.log(err);
@@ -76,7 +76,7 @@ router.post("/bak/baks", function(req, res, next) {
     var str = req.get('Authorization');
     // console.log(req);
     try {
-        // var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
+        var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
         db.query("SELECT DISTINCT unit FROM bak_log INNER JOIN bak_info ON bak_log.bak_id = bak_info.id WHERE \
             DATE(bak_log.date_added) = str_to_date(?, '%d/%m/%Y');", 
             [req.body.date], function(err, result) {
@@ -94,7 +94,7 @@ router.post("/semaian/semaians", function(req, res, next) {
     var str = req.get('Authorization');
     // console.log(req);
     try {
-        // var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
+        var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
         db.query("SELECT DISTINCT CONCAT(name, ' - ', batch_no) AS unit FROM semaian_log INNER JOIN semaian_info ON semaian_log.semaian_id = semaian_info.id WHERE \
             DATE(semaian_log.date_added) = str_to_date(?, '%d/%m/%Y');", 
             [req.body.date], function(err, result) {
@@ -113,7 +113,7 @@ router.post("/bak/times", function(req, res, next) {
     var str = req.get('Authorization');
     // console.log(req);
     try {
-        // var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
+        var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
         db.query("SELECT DISTINCT bak_log.id, TIME(bak_log.date_added) FROM bak_log \
             INNER JOIN bak_info ON bak_log.bak_id = bak_info.id WHERE \
             (DATE(bak_log.date_added), unit) = (str_to_date(?, '%d/%m/%Y'), ?);", 
@@ -133,7 +133,7 @@ router.post("/semaian/times", function(req, res, next) {
     var str = req.get('Authorization');
     // console.log(req);
     try {
-        // var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
+        var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
         db.query("SELECT DISTINCT semaian_log.id, TIME(semaian_log.date_added) FROM semaian_log \
             INNER JOIN semaian_info ON semaian_log.semaian_id = semaian_info.id WHERE \
             (DATE(semaian_log.date_added), CONCAT(name, ' - ', batch_no)) = (str_to_date(?, '%d/%m/%Y'), ?);", 
@@ -153,7 +153,7 @@ router.get("/bak/bak_id/:id", function(req, res, next) {
     var str = req.get('Authorization');
     // console.log(req);
     try {
-        // var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
+        var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
         db.query('SELECT bak_log.*, users.name AS username, bak_info.unit \
             from bak_log INNER JOIN users ON bak_log.user_id = users.id \
             INNER JOIN bak_info ON bak_log.bak_id = bak_info.id WHERE bak_log.id = ?;',
@@ -172,7 +172,7 @@ router.get("/semaian/semaian_id/:id", function(req, res, next) {
     var str = req.get('Authorization');
     // console.log(req);
     try {
-        // var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
+        var jwt_info = jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
         db.query('SELECT semaian_log.*, users.name AS username, semaian_info.merek_seed, semaian_info.batch_no, semaian_info.jumlah_awal \
             from semaian_log INNER JOIN users ON semaian_log.user_id = users.id \
             INNER JOIN semaian_info ON semaian_log.semaian_id = semaian_info.id WHERE semaian_log.id = ?;',
