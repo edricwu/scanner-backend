1b787640cfe754e86ecfbd00b36e56c7c68b22f9
edricwu <edricwu008@gmail.com>
2020-11-22 17:05:05 +0800

    add user limit:

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,8 @@ var jwt = require('jsonwebtoken');
 
 var db = require('../config/mysql');
 
+var user_limit = {1: 3, 2: 10, 3: 20};
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     var str = req.get('Authorization');
@@ -54,18 +56,28 @@ router.post('/add_user', function (req, res, next) {
         var jwt_info =jwt.verify(str, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' });
         db.query("SELECT level FROM users WHERE id = ?", jwt_info["id"], function (err, row) {
             if (row[0]["level"] < 3) {
-                db.query("SELECT * FROM users WHERE name = ? AND deleted = false", req.body.name, function (err, row) {
-                    if (row.length != 0) {
-                        console.log(`User with name ${req.body.name} already exists`);
-                        res.status(409);
-                        res.send("A user with that name already exists");
+                db.query("SELECT COUNT(*) AS num_user FROM greenfeast.users WHERE deleted = false AND level = ?;", req.body.level, 
+                function(err, row) {
+                    if (row[0]["num_user"] >= user_limit[req.body.level]) {
+                        res.status(405);
+                        res.send("User limit has been reached");
                     }
                     else {
-                        var password = crypto.createHash('sha256').update(req.body.password).digest('hex');
-                        db.query("INSERT INTO users (name, password, level) VALUES (?, ?, ?)",
-                            [req.body.name, password, req.body.level],
-                        );
-                        res.send("A new user has been created");
+                        db.query("SELECT * FROM users WHERE name = ? AND deleted = false", req.body.name, function (err, row) {
+                            if (row.length != 0) {
+                                console.log(`User with name ${req.body.name} already exists`);
+                                res.status(409);
+                                res.send("A user with that name already exists");
+                            }
+                            else {
+                                var password = crypto.createHash('sha256').update(req.body.password).digest('hex');
+                                db.query("INSERT INTO users (name, password, level) VALUES (?, ?, ?)",
+                                    [req.body.name, password, req.body.level], function(err, row) {
+                                        res.send("A new user has been created");
+                                    }
+                                );
+                            }
+                        })
                     }
                 })
             }
@@ -89,32 +101,41 @@ router.post('/manage_user', function (req, res, next) {
             if (row0[0]["level"] < 3) {
                 db.query("SELECT level FROM users WHERE id = ?", req.body.id, function (err, row1) {
                     if (row0[0]["level"] <= row1[0]["level"]) {
-                        db.query("SELECT * FROM users WHERE name = ? AND deleted = false AND id != ?", [req.body.name, req.body.id], function (err, row) {
-                            if (row.length != 0) {
-                                console.log(`User with name ${req.body.name} already exists`);
-                                res.status(409);
-                                res.send("A user with that name already exists");
-                            }
-                            else {
-                                if (req.body.password != "") {
-                                    var password = crypto.createHash('sha256').update(req.body.password).digest('hex');
-                                    db.query("UPDATE users SET name = ?, password = ?, level = ? WHERE id = ?",
-                                        [req.body.name, password, req.body.level, req.body.id], function (err, row) {
-                                            console.log(err);
-                                            res.send("User has been modified");
-                                        }
-                                    )
+                        db.query("SELECT COUNT(*) AS num_user FROM greenfeast.users WHERE deleted = false AND level = ? AND id != ?;",
+                            [req.body.level, req.body.id], function(err, row) {
+                                if (row[0]["num_user"] >= user_limit[req.body.level]) {
+                                    res.status(405);
+                                    res.send("User limit has been reached");
                                 }
                                 else {
-                                    db.query("UPDATE users SET name = ?, level = ? WHERE id = ?",
-                                        [req.body.name, req.body.level, req.body.id], function (err, row) {
-                                            console.log(err);
-                                            res.send("User has been modified");
+                                    db.query("SELECT * FROM users WHERE name = ? AND deleted = false AND id != ?", [req.body.name, req.body.id], function (err, row) {
+                                        if (row.length != 0) {
+                                            console.log(`User with name ${req.body.name} already exists`);
+                                            res.status(409);
+                                            res.send("A user with that name already exists");
                                         }
-                                    )
+                                        else {
+                                            if (req.body.password != "") {
+                                                var password = crypto.createHash('sha256').update(req.body.password).digest('hex');
+                                                db.query("UPDATE users SET name = ?, password = ?, level = ? WHERE id = ?",
+                                                    [req.body.name, password, req.body.level, req.body.id], function (err, row) {
+                                                        console.log(err);
+                                                        res.send("User has been modified");
+                                                    }
+                                                )
+                                            }
+                                            else {
+                                                db.query("UPDATE users SET name = ?, level = ? WHERE id = ?",
+                                                    [req.body.name, req.body.level, req.body.id], function (err, row) {
+                                                        console.log(err);
+                                                        res.send("User has been modified");
+                                                    }
+                                                )
+                                            }
+                                        }
+                                    })
                                 }
-                            }
-                        })
+                            })
                     }
                     else {
                         res.status(403);
